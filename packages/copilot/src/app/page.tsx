512a4283120bb557cea401e412e8274f11f02ff9
David Britt <djmb102@gmail.com>
2025-10-09 23:23:47 +0200

    add copilotkit

---
+++ packages/copilot/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { CopilotSidebar } from "@copilotkit/react-ui";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Your App</h1>
+      <CopilotSidebar /> // [!code highlight]
+    </main>
+  );
+}
